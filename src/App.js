f5feb9e675c1bb9c7758a1f49fdb8f640b6825d1
kaije5 <53300974+kaije5@users.noreply.github.com>
2021-03-09 10:44:59 +0100

    Implement navbar

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import NavBar from './components/NavBar'
+import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <NavBar></NavBar>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import CustomBtn from './CustomBtn'
+import {Toolbar, Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles"; 
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor: "#fff",
+        ['@media (max-width:780px)']: { 
+           flexDirection: "column"
+          }
+    },
+    menuItem: {
+        cursor: "pointer", 
+        flexGrow: 1,
+        "&:hover": {
+            color:  "#4f25c8"
+        },
+        ['@media (max-width:780px)']: { 
+            paddingBottom: "1rem"    }
+    }
+})
+
+function NavBar() {
+    const classes = styles()
+    return (
+            <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
+                <Typography variant="h6" className={classes.menuItem}>
+                   Webhosting
+                </Typography>
+                <Typography variant="h6" className={classes.menuItem}>
+                    Dedicated Webhosting
+                </Typography>
+                <CustomBtn txt="Contact"/>
+            </Toolbar>
+    )
+}
+
+export default NavBar
