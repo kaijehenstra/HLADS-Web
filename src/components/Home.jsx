255bbd23acd41e0e17e32bc3defb36902840e17b
kaije5 <53300974+kaije5@users.noreply.github.com>
2021-03-10 15:51:24 +0100

    Finally fix the body text *sigh*

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Typography} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+
+const styles = makeStyles({
+    wrapper: {
+       display: "flex",
+       flexDirection: "column", 
+       alignItems: "center", 
+       padding: "0 5rem 0 5rem"
+    }, 
+})
 
 function Home() {
+    const classes = styles(); 
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={classes.wrapper}>
+            <Typography className={classes.item} variant="h1">Home</Typography>
         </div>
     )
 }
